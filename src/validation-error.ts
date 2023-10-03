0652526de7fcb42cc64032be9edee019742cc34f
Giorgi Kobulashvili <gkio@gkios-MacBook-Pro.local>
2023-10-03 15:45:05 +0300

    fix formating

--- src/validation-error.ts
+++ src/validation-error.ts
@@ -1,13 +1,13 @@
 export class ValidationError extends Error {
-  private fieldErrors: Map<string, string>;
+  private fieldErrors: Map<string, string>
 
   constructor(message: string, fieldErrors: Map<string, string>) {
-    super(message);
-    this.fieldErrors = fieldErrors;
-    Error.captureStackTrace(this, this.constructor);
+    super(message)
+    this.fieldErrors = fieldErrors
+    Error.captureStackTrace(this, this.constructor)
   }
 
   getFieldError(): Map<string, string> {
-    return this.fieldErrors;
+    return this.fieldErrors
   }
 }
