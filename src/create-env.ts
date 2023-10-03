74801b3f655a363d3636fe73a31ef67e8f5c7bf6
Giorgi Kobulashvili <gkio@gkios-MacBook-Pro.local>
2023-10-03 15:45:05 +0300

    fix formating

--- src/create-env.ts
+++ src/create-env.ts
@@ -1,64 +1,83 @@
-import { groupVariables } from './group-variables.js';
-import { ValidationError } from './validation-error.js';
+import { groupVariables } from './group-variables.js'
+import { ValidationError } from './validation-error.js'
 
-import type { z } from 'zod';
-import type {Group} from "./group.js";
+import type { z } from 'zod'
+import type { Group } from './group.js'
 
-export function createEnv<TInput, TOutput extends object, TGroupedOutput = Group<TOutput>>(
+export function createEnv<
+  TInput,
+  TOutput extends object,
+  TGroupedOutput = Group<TOutput>,
+>(
   processEnv: NodeJS.ProcessEnv | Record<string, unknown>,
   schema: z.ZodType<TOutput, z.ZodTypeDef, TInput>,
 ): TGroupedOutput {
-  const result = schema.safeParse(processEnv);
+  const result = schema.safeParse(processEnv)
 
   if (result.success) {
-    return groupVariables(result.data, '.');
+    return groupVariables(result.data, '.')
   }
 
-  handleValidationErrors(result.error.errors);
+  handleValidationErrors(result.error.errors)
 }
 
 function handleValidationErrors(errors: z.ZodError['errors']): never {
-  const affectedVariables: string[] = [];
-  const fieldErrors: Map<string, string> = new Map();
+  const affectedVariables: string[] = []
+  const fieldErrors: Map<string, string> = new Map()
 
   errors.forEach((error) => {
-    const fieldName = error.path[0] as string;
-    affectedVariables.push(fieldName);
-    fieldErrors.set(fieldName, generateErrorMessage(fieldName, error));
-  });
+    const fieldName = error.path[0] as string
+    affectedVariables.push(fieldName)
+    fieldErrors.set(fieldName, generateErrorMessage(fieldName, error))
+  })
 
-  throw new ValidationError(`Validation of the following environment variables failed: "${affectedVariables.join('", "')}"`, fieldErrors);
+  throw new ValidationError(
+    `Validation of the following environment variables failed: "${affectedVariables.join(
+      '", "',
+    )}"`,
+    fieldErrors,
+  )
 }
 
 function generateErrorMessage(fieldName: string, error: z.ZodIssue): string {
-  let errorMessage = `Unknown error for variable "${fieldName}"`;
+  let errorMessage = `Unknown error for variable "${fieldName}"`
 
   switch (error.code) {
     case 'invalid_type':
-      errorMessage = error.received === 'undefined'
-        ? `Missing variable: The variable "${fieldName}" is required, but missing`
-        : `The value of variable "${fieldName}" must be of type "${error.expected}", but received "${error.received}"`;
-      break;
+      errorMessage =
+        error.received === 'undefined'
+          ? `Missing variable: The variable "${fieldName}" is required, but missing`
+          : `The value of variable "${fieldName}" must be of type "${error.expected}", but received "${error.received}"`
+      break
     case 'invalid_enum_value':
-      errorMessage = `Enum mismatch: The variable "${fieldName}" must be one of the following values: "${error.options.join('", "')}", but received "${error.received}"`;
-      break;
+      errorMessage = `Enum mismatch: The variable "${fieldName}" must be one of the following values: "${error.options.join(
+        '", "',
+      )}", but received "${error.received}"`
+      break
     case 'invalid_string':
-      errorMessage = generateStringErrorMessage(fieldName, error);
-      break;
+      errorMessage = generateStringErrorMessage(fieldName, error)
+      break
   }
 
-  return errorMessage;
+  return errorMessage
 }
 
-function generateStringErrorMessage(fieldName: string, error: z.ZodInvalidStringIssue): string {
+function generateStringErrorMessage(
+  fieldName: string,
+  error: z.ZodInvalidStringIssue,
+): string {
   if (typeof error.validation === 'string') {
-    return `Format error: The value of variable "${fieldName}" must be a valid ${error.validation}`;
+    return `Format error: The value of variable "${fieldName}" must be a valid ${error.validation}`
   }
   if ('startsWith' in error.validation) {
-    return `Format error: The value of variable "${fieldName}" must start with ${error.validation.startsWith}`;
+    return `Format error: The value of variable "${fieldName}" must start with ${error.validation.startsWith}`
   }
   if ('endsWith' in error.validation) {
-    return `Format error: The value of variable "${fieldName}" must end with ${error.validation.endsWith}`;
+    return `Format error: The value of variable "${fieldName}" must end with ${error.validation.endsWith}`
   }
-  return `Format error: The value of variable "${fieldName}" must include "${error.validation.includes}"${error.validation.position ? ` at position ${error.validation.position}` : ''}`;
+  return `Format error: The value of variable "${fieldName}" must include "${
+    error.validation.includes
+  }"${
+    error.validation.position ? ` at position ${error.validation.position}` : ''
+  }`
 }
--- src/group-variables.ts
+++ src/group-variables.ts
@@ -1,19 +1,24 @@
-import {camelCase} from './utils/camel-case.js';
+import { camelCase } from './utils/camel-case.js'
 
-import type {Group} from './group.js';
+import type { Group } from './group.js'
 
-export function groupVariables<TInput extends object, TOutput = Group<TInput, '.'>>(input: TInput, groupBy = '.'): TOutput {
+export function groupVariables<
+  TInput extends object,
+  TOutput = Group<TInput, '.'>,
+>(input: TInput, groupBy = '.'): TOutput {
   return Object.entries(input).reduce((output: TOutput, [key, value]) => {
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    key.split(groupBy).reduce((target: any, part: string, index: number, parts: string[]) => {
-      const camelCasedPart = camelCase(part) as keyof TOutput;
-      if (index === parts.length - 1) {
-        target[camelCasedPart] = value;
-      } else {
-        target[camelCasedPart] = target[camelCasedPart] || {};
-      }
-      return target[camelCasedPart];
-    }, output);
-    return output;
-  }, {} as TOutput);
+    key
+      .split(groupBy)
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      .reduce((target: any, part: string, index: number, parts: string[]) => {
+        const camelCasedPart = camelCase(part) as keyof TOutput
+        if (index === parts.length - 1) {
+          target[camelCasedPart] = value
+        } else {
+          target[camelCasedPart] = target[camelCasedPart] || {}
+        }
+        return target[camelCasedPart]
+      }, output)
+    return output
+  }, {} as TOutput)
 }
